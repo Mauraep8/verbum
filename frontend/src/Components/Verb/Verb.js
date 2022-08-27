efbc71dfb018e565ac755e0f291a46488021e691
Mauraep8 <67877541+Mauraep8@users.noreply.github.com>
2022-08-27 08:25:09 -0400

    added dockerfile and .yml file.Added open/close functionality to dropdown menu btns

--- frontend/src/Components/Verb/Verb.js
+++ frontend/src/Components/Verb/Verb.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useEffect} from 'react'
+import React, {useContext, useState, useEffect, useRef} from 'react'
 import { ThemeContext } from '../../Pages/French/FrenchPage';
 import DropMenuVerb from '../DropMenu/DropMenuVerb';
 import './Verb.scss'
@@ -14,12 +14,27 @@ export default function Verb() {
     useEffect(()=>{
       setDropList(value.state)
     },[value.state]) 
+  
+  //DROPMENU OPEN CLOSE FUNC
+  const dropmenuWrapper = useRef ([])
 
+  const handlerDropmenu = () =>{
+    if (dropmenuWrapper.current.classList.value === 'verb__dropmenu-wrapper--hidden'){
+      dropmenuWrapper.current.classList.remove('verb__dropmenu-wrapper--hidden')
+      dropmenuWrapper.current.classList.add('verb__dropmenu-wrapper--active')
+    } else {
+      dropmenuWrapper.current.classList.remove('verb__dropmenu-wrapper--active')
+      dropmenuWrapper.current.classList.add('verb__dropmenu-wrapper--hidden')
+
+    }
+  }
 
   return (
     <div className='verb'>
-        <button className='verb__button'>{dropList[0]}</button>
-        <DropMenuVerb dropList={dropList}/>
+        <button className='verb__button' onClick={handlerDropmenu}>{dropList[0]}</button>
+        <div className='verb__dropmenu-wrapper--hidden' ref={dropmenuWrapper}>
+          <DropMenuVerb dropList={dropList}/>
+        </div>
     </div>
   )
 }
