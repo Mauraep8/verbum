64febef3944d193b5ae21deffe421cc73a69dd42
Mauraep8 <67877541+Mauraep8@users.noreply.github.com>
2022-10-07 14:26:55 -0400

    Database user and verb lists are now alphabetically sorted when verbs are added or deleted

--- frontend/src/Store/databaseSlice.js
+++ frontend/src/Store/databaseSlice.js
@@ -14,31 +14,59 @@ const getVerbList = () =>{
 }
 getVerbList()
 
+// INITIAL STATE OF VERBLIBRARY AND USERLIBRARY
 const initialState = {
     verbLibrary: [{verbName: 'aller', id: 1}, {verbName: 'bouger', id: 2}, {verbName: 'dormir', id: 3}],
     userLibrary: [{verbName: 'avoir', id: 4}, {verbName: 'etre', id: 5}, {verbName: 'finir', id: 6}]
 }
 
+// SORT LISTS ALPHABETICALLY AFTER VERB IS ADDED OR DELETED
+const compare = (a,b) => {
+    const verbA = a.verbName.toUpperCase()
+    const verbB = b.verbName.toUpperCase()
+
+    let comparison = 0
+    if (verbA > verbB) {
+        comparison = 1
+
+    } else if (verbA < verbB){
+        comparison = -1
+    }
+    return comparison
+}
+
+// DATABASE SLICE
 const databaseSlice = createSlice({
     name: 'database',
     initialState,
     reducers: {
         verbAdded: (state, action)=>{
             
+            // PUSH NEW VERB INTO USERLIST
             state.userLibrary.push({verbName:action.payload})
             const indexVerb = state.verbLibrary.findIndex(verb =>{
                 return verb.verbName === action.payload
             })
-            state.verbLibrary.splice(indexVerb, 1)
 
+            // SORT USERLIST ALPHABETICALLY
+            state.userLibrary.sort(compare)
+
+            // REMOVE ADDED VERB FROM VERBLIBRARY
+            state.verbLibrary.splice(indexVerb, 1)
         },
     
         verbDeleted: (state, action)=>{
-            console.log(action)
+
+            // PUSH NEW VERB INTO VERBLIBRARY
             state.verbLibrary.push({verbName:action.payload})
             const indexVerb = state.userLibrary.findIndex(verb =>{
                 return verb.verbName === action.payload
             })
+
+            // SORT VERBLIBRARY ALPHABETICALLY
+            state.verbLibrary.sort(compare)
+
+            // REMOVE ADDED VERB FROM USERLIST
             state.userLibrary.splice(indexVerb, 1)
         }
     }
