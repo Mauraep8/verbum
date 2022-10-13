56d9b03ea016f51821c4ce7ae6c2f01dfd1020c3
Mauraep8 <67877541+Mauraep8@users.noreply.github.com>
2022-10-12 21:53:44 -0400

    added new column to DB for the initial verbs of userList. Added new initial state for userList. Now verbLibrary and userList contain the verbs as full objects with all properties, instead of just single strings. Also dispatch functions send verbs as objects to keep consistency throughout the app

--- frontend/src/Components/VerbList/VerbList.js
+++ frontend/src/Components/VerbList/VerbList.js
@@ -12,7 +12,13 @@ export default function VerbList(props)  {
                     return <ListEntry
                     key={singleVerb.id}
                     id={singleVerb.id}
-                    verbName={singleVerb.verb}
+                    verbName={singleVerb.verbName}
+                    verbGroup={singleVerb.verbGroup}
+                    bescherelleId={singleVerb.bescherelleId}
+                    primaryVerb={singleVerb.primaryVerb}
+                    specialVerb={singleVerb.specialVerb}
+                    auxiliaryVerb={singleVerb.auxiliaryVerb}
+                    initialVerb={singleVerb.initialVerb}
                     buttonAction={'add'}/>
                 })}
             </div>
