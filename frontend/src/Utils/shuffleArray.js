ed71150a2be7fe7169da4e3b4832ffb6e991cd58
Mauraep8 <67877541+Mauraep8@users.noreply.github.com>
2022-11-14 17:46:54 -0500

    moved state of grammar features from the Exercise component to the Store. This solved dispatch issue in useEffect as well. Completed full grammar logic of the imperative mood and its effects on other grammatical features

--- frontend/src/Utils/shuffleArray.js
+++ frontend/src/Utils/shuffleArray.js
@@ -1,5 +1,9 @@
 // FUNCTION SHUFFLES ARRAY AND RETURNS IN AN OBJECT THE RESULT WITH ITS SHUFFLE COLOR STATUS 
 export const shuffleArray = (array) =>{
+        if(array === null){
+                const object = {result: 'none', colorChange: false}
+                return object
+        }
         if (array.length === 0){
                 const object = {result: null, colorChange: false}
                 return object
