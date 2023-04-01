aa94442da086e76e83bfde2fe6a3622719c58150
Mauraep8 <67877541+Mauraep8@users.noreply.github.com>
2023-04-01 16:45:51 -0400

    readded blue color change functionality to dropmenuButton

--- frontend/src/Components/DropMenu/DropMenu.js
+++ frontend/src/Components/DropMenu/DropMenu.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useRef, useState} from 'react'
 import DropMenuButton from '../DropMenuButton/DropMenuButton'
 import DropMenuList from '../DropMenuList/DropMenuList'
+import {useDispatch} from 'react-redux'
 import "./DropMenu.scss";
+import { shuffleCleared } from '../../Store/exerciseSlice';
 
 
 
@@ -9,12 +11,25 @@ export default function DropMenu(props) {
 
     const [showDropmenu, setShowDropmenu] = useState(false)
     const buttonRef = useRef(null)
+    const menuRef = useRef(null)
+    const dispatch = useDispatch()
 
     useEffect(() => {
       const handler = (e) => {        
         if (buttonRef.current && !buttonRef.current.contains(e.target)){
         setShowDropmenu(false)
+
     }}
+
+    if (showDropmenu===true){
+        menuRef.current.classList.add('dropmenu__menu--visible')
+        menuRef.current.classList.remove('dropmenu__menu--hidden')
+        dispatch(shuffleCleared())
+    } else {
+        menuRef.current.classList.remove('dropmenu__menu--visible')
+        menuRef.current.classList.add('dropmenu__menu--hidden')
+        dispatch(shuffleCleared())
+    }
 
       window.addEventListener('click', handler)
     
@@ -30,9 +45,9 @@ export default function DropMenu(props) {
     return (
         <div className='dropmenu'>
             <DropMenuButton type={props.type} result={props.result} ref={buttonRef} colorChange={props.colorChange} function={handleInputClick}/>
-            {showDropmenu && <div className='dropmenu__menu'>
+            <div className='dropmenu__menu' ref={menuRef}>
                 <DropMenuList list={props.list} type={props.type}/>
-            </div>}
+            </div>
         </div>
   )
 }
--- frontend/src/Components/DropMenuButton/DropMenuButton.js
+++ frontend/src/Components/DropMenuButton/DropMenuButton.js
@@ -6,28 +6,27 @@ import './DropMenuButton.scss'
 const DropMenuButton = forwardRef(function DropMenuButton(props, ref) {
  
   const shuffleAction = useSelector(((state)=> state.exercise.shuffleAction))
-  const dropmenuButton = useRef ([])
 
   const clickHandler = () =>{
     props.function()
   }
 
   // WHEN SHUFFLE IS CLICKED BUTTONS TURN BLUE MOMENTARILY
-  // if (shuffleAction === true){
-  //   if (props.colorChange === true){
-  //       ref.current.classList.remove('dropmenuButton__button--inactive')
-  //       ref.current.classList.add('dropmenuButton__button--active')
+  if (shuffleAction === true){
+    if (props.colorChange === true){
+        ref.current.classList.remove('dropmenuButton__button--inactive')
+        ref.current.classList.add('dropmenuButton__button--active')
                 
-  //       setTimeout(() => {
-  //         ref.current.classList.add('dropmenuButton__button--inactive')
-  //         ref.current.classList.remove('dropmenuButton__button--active')               
-  //       }, 400);     
-  //   }
-  // }   
+        setTimeout(() => {
+          ref.current.classList.add('dropmenuButton__button--inactive')
+          ref.current.classList.remove('dropmenuButton__button--active')               
+        }, 400);     
+    }
+  }   
 
   return (
     <div className='dropmenuButton'>
-        <label className='dropmenuButton__label' htmlFor="">{props.type}</label>
+        <label className='dropmenuButton__label'>{props.type}</label>
         <button className={`dropmenuButton__button dropmenuButton__button--${props.type}`} ref={ref} onClick={clickHandler}>{props.result}</button>
         <i className="bi-caret-down-fill"></i>
   </div>
