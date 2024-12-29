8e7c2a6cf363bcdf4b4000b5c74cd763c317d49b
Mauraep8 <67877541+Mauraep8@users.noreply.github.com>
2024-12-29 13:50:47 -0500

    fixed styling, proportions and positionings in mid-tablet and desktop mode. Additionally deleted all the comment out code of all components

--- frontend/src/Utils/grammarLogic/conditional/conditionalConditions.js
+++ frontend/src/Utils/grammarLogic/conditional/conditionalConditions.js
@@ -1,24 +1,7 @@
 import {
   conditional,
-  feminin,
-  firstPerson,
-  futurAnterieur,
-  futurSimple,
-  imparfait,
-  imperative,
-  indicative,
-  masculin,
   passe,
-  passeAnterieur,
-  passeCompose,
-  passeSimple,
-  plural,
-  plusQueParfait,
   present,
-  secondPerson,
-  singular,
-  subjunctive,
-  thirdPerson,
 } from "../../grammarTerms";
 
 export default function conditionalConditions(tenseArray) {
